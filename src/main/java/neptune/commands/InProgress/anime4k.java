28a17d8ade28ddc1bb9a7134f32dd1fb7139b520
Codel1417 <austinrichards98@gmail.com>
2020-10-12 17:28:57 -0700

    Hook into another implamentation of anime4k

--- src/main/java/neptune/commands/InProgress/anime4k.java
+++ src/main/java/neptune/commands/InProgress/anime4k.java
@@ -1,27 +1,25 @@
 package neptune.commands.InProgress;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import javax.imageio.stream.MemoryCacheImageOutputStream;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import com.bloc97.anime4k.ImageKernel;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import neptune.commands.CommandInterface;
 import neptune.commands.commandCategories;
 import neptune.storage.guildObject;
-import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.entities.Message.Attachment;
+
 
 public class anime4k implements CommandInterface {
     protected static final Logger log = LogManager.getLogger();
-    ImageKernel kernel = new ImageKernel();
+    File anime4kPath = new File("Anime4KCPP_CLI" + File.separator + "Anime4KCPP_CLI.exe");
 
     @Override
     public String getName() {
@@ -65,92 +63,42 @@ public class anime4k implements CommandInterface {
 
     @Override
     public guildObject run(GuildMessageReceivedEvent event, String messageContent, guildObject guildEntity) {
+        File directory = new File("tmp"+ File.separator + event.getMessageId() + File.separator);
+
+        File originalImage;
+        File outputImage;
         List<Attachment> attachments = event.getMessage().getAttachments();
         if (!attachments.isEmpty()){
-            Attachment image = attachments.get(0);    
-            BufferedImage img;
-
-            //This attempts to get the final image url after redirects
-            HttpURLConnection connection;
-            String finalUrl = image.getUrl();
-            try {
-                do {
-                    connection = (HttpURLConnection) new URL(finalUrl)
-                            .openConnection();
-                    connection.setInstanceFollowRedirects(false);
-                    connection.setUseCaches(false);
-                    connection.setRequestMethod("GET");
-                    connection.connect();
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode >= 300 && responseCode < 400) {
-                        String redirectedUrl = connection.getHeaderField("Location");
-                        if (null == redirectedUrl)
-                            break;
-                        finalUrl = redirectedUrl;
-                    } else
-                        break;
-                } while (connection.getResponseCode() != HttpURLConnection.HTTP_OK);
-                connection.disconnect();
-
-
-                img = ImageIO.read(new URL(finalUrl));
-
-                float scale = 2f;
-                float pushStrength = scale / 6f;
-                float pushGradStrength = scale / 2f;
-
-                //upscale image
-                img = scale(img, (int)(img.getWidth() * scale), (int)(img.getHeight() * scale));
-
-
-                //enhance image
-                kernel.setPushStrength(pushStrength);
-                kernel.setPushGradStrength(pushGradStrength);
-                kernel.setBufferedImage(img);
-                kernel.process();
-                kernel.updateBufferedImage();
-
-                //prepare for discord
-                ByteArrayOutputStream writerOutput = new ByteArrayOutputStream();
-                MemoryCacheImageOutputStream imageOutputStream = new MemoryCacheImageOutputStream(writerOutput);
-                byte[] byteOutput;
-                ImageIO.write(img, "png", imageOutputStream);
-                byteOutput = writerOutput.toByteArray();
-
-                //downscale image until i can upload it
-                while (byteOutput.length > 8388608)  {
-                    byteOutput = null;
-                    writerOutput = new ByteArrayOutputStream();
-                    imageOutputStream = new MemoryCacheImageOutputStream(writerOutput);
-                    log.warn("Downscaling image");
-                    img = scale(img, (int)(img.getWidth() * 0.95), (int)(img.getHeight() * 0.95));
-                    ImageIO.write(img, "png", imageOutputStream);
-                    byteOutput = writerOutput.toByteArray();
-                    log.warn(byteOutput.length);
-                } 
-
-                event.getChannel().sendMessage("Here you go").addFile(byteOutput,"upscaled.png").queue();
-
-
-            } catch (IOException e1) {
-                log.error(e1);
-                event.getChannel().sendMessage("Unable to download image").queue();
-                return guildEntity;
-            }
+            Attachment image = attachments.get(0);
+            originalImage = new File(directory, "original" + image.getFileExtension());
+            outputImage = new File(directory, "output" + image.getFileExtension());
+            image.downloadToFile(originalImage).complete(originalImage);
         }
-        else {event.getChannel().sendMessage("Please attach an immage with this command.").queue();}
-        return guildEntity;
+        else return guildEntity;
+
+        CommandLine cmdl = new CommandLine(anime4kPath);
+
+        cmdl.addArgument("-i '" + originalImage.getAbsolutePath() + "'");
+        cmdl.addArgument("-o '" + outputImage.getAbsolutePath()+ "'");
+        cmdl.addArgument("-q"); //use gpu
+
+        int exitValue = 1;
+        DefaultExecutor executor = new DefaultExecutor();
+        try {
+            exitValue = executor.execute(cmdl);
+        } catch (IOException e) {
+            log.error(e);
+            return guildEntity;
+        }
+        if (exitValue == 0){
+            event.getChannel().sendFile(outputImage).complete();
+        }
+        try {
+            FileUtils.deleteDirectory(directory);
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return guildEntity; 
     }
-    static BufferedImage copyType(BufferedImage bi) {
-        BufferedImage newImage = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        newImage.getGraphics().drawImage(bi, 0, 0, null);
-        return newImage;
-    }
-    static BufferedImage scale(BufferedImage bi, int width, int height) {
-        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = (Graphics2D)newImage.getGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        g2.drawImage(bi, 0, 0, width, height, null);
-        return newImage;
-    }
+    
 }
