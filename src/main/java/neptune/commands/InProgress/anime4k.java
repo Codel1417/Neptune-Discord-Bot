d7c0fdd32839abfff1e77139865762b344e45b1c
Codel1417 <austinrichards98@gmail.com>
2020-10-12 22:21:38 -0700

    Disable antialiasing

--- src/main/java/neptune/commands/InProgress/anime4k.java
+++ src/main/java/neptune/commands/InProgress/anime4k.java
@@ -168,7 +168,9 @@ public class anime4k implements CommandInterface {
         BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = (Graphics2D)newImage.getGraphics();
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+        g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
         g2.drawImage(bi, 0, 0, width, height, null);
         return newImage;
     }
