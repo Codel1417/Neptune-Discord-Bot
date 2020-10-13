1b7de55b00ac1e159bc9cb75c009836c74f8a8f7
Codel1417 <austinrichards98@gmail.com>
2020-10-12 20:35:56 -0700

    cries in opencv

--- src/main/java/neptune/commands/InProgress/anime4k.java
+++ src/main/java/neptune/commands/InProgress/anime4k.java
@@ -20,11 +20,7 @@ import neptune.storage.guildObject;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.entities.Message.Attachment;
 
-public class anime4k implements CommandInterface {
-    static {
-        nu.pattern.OpenCV.loadShared();
-        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
-    }    
+public class anime4k implements CommandInterface {   
     protected static final Logger log = LogManager.getLogger();
     File anime4kPath = new File("Anime4KCPP_CLI" + File.separator + "Anime4KCPP_CLI.exe");
 
@@ -70,6 +66,8 @@ public class anime4k implements CommandInterface {
 
     @Override
     public guildObject run(GuildMessageReceivedEvent event, String messageContent, guildObject guildEntity) {
+        nu.pattern.OpenCV.loadShared();
+
         File directory = new File("tmp" + File.separator + event.getMessageId() + File.separator);
         File originalImage;
         File outputImage;
