be9f7ee4d6141af1ba2c753e74d3a0bdd4f911e8
Codel1417 <austinrichards98@gmail.com>
2020-10-12 22:12:30 -0700

    Tweak settings

--- src/main/java/neptune/commands/InProgress/anime4k.java
+++ src/main/java/neptune/commands/InProgress/anime4k.java
@@ -169,8 +169,6 @@ public class anime4k implements CommandInterface {
         Graphics2D g2 = (Graphics2D)newImage.getGraphics();
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g2.drawImage(bi, 0, 0, width, height, null);
         return newImage;
     }
