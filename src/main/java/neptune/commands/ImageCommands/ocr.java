158e0af1d4a282a480dad8b1182c749579e99965
Codel1417 <austinrichards98@gmail.com>
2021-05-01 22:41:07 +0000

    Fix Tesseract

--- src/main/java/neptune/commands/ImageCommands/ocr.java
+++ src/main/java/neptune/commands/ImageCommands/ocr.java
@@ -14,33 +14,28 @@ import javax.imageio.ImageIO;
 
 public class ocr implements ICommand {
     protected static final Logger log = LogManager.getLogger();
-    String tessdata = "dependencies" + File.separator + "tessdata";
-    Helpers helpers = new Helpers();
+    private String tessdata = File.separator + "tessdata";
+    private Helpers helpers = new Helpers();
+    private Tesseract tesseract = new Tesseract();
 
     public ocr(){
+        tesseract.setDatapath(tessdata);
         File tessCheck = new File(tessdata);
         if (!tessCheck.exists()){
             log.fatal("Tessdata not found");
         }
     }
     @Override
-    public void run(
-            GuildMessageReceivedEvent event, String messageContent) {
-        Tesseract tesseract = new Tesseract();
-        tesseract.setOcrEngineMode(1);
+    public void run(GuildMessageReceivedEvent event, String messageContent) {
         try {
             String ImageUrl = helpers.getImageUrl(event);
             if (ImageUrl != null) {
                 BufferedImage img = ImageIO.read(new URL(ImageUrl));
-                BufferedImage greyImage =
-                        new BufferedImage(
-                                img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+                BufferedImage greyImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
                 Graphics g = greyImage.getGraphics();
                 g.drawImage(img, 0, 0, null);
                 g.dispose();
-
                 // if this file does not exist java will crash
-                tesseract.setDatapath(tessdata);
                 String text = tesseract.doOCR(greyImage);
                 event.getChannel().sendMessage(text).queue();
             }
