09484a4c0ac57efcfa344fa92c9d6dd8ab140fcf
codel1417 <austinrichards98@gmail.com>
2021-08-28 00:21:11 -0700

    java security manager fix

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 jstatd \
     -p 1233 \
     -n Neptune \
-    -J-Djava.security.policy=all.policy \
-    -J-Djava.rmi.server.logCalls=true &
+    -J-Djava.rmi.server.logCalls=true \
+    -J-Djava.security.policy=tools.policy &
 java \
     -Dcom.sun.management.jmxremote.ssl=false \
     -Dcom.sun.management.jmxremote.port=1232 \
