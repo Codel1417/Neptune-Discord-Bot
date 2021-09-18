546ccaa4b62fc27b5ee701782b0fcbdab20d29e1
codel1417 <austinrichards98@gmail.com>
2021-09-17 22:53:29 -0700

    Apparently something needs this

--- src/main/java/neptune/storage/Enum/GuildOptionsEnum.java
+++ src/main/java/neptune/storage/Enum/GuildOptionsEnum.java
@@ -4,5 +4,6 @@ public enum GuildOptionsEnum {
     LoggingEnabled,
     LeaderboardLevelUpNotification,
     leaderboardEnabled,
-    customSounds
-}
+    customSounds,
+    CustomRoleEnabled
+    }
